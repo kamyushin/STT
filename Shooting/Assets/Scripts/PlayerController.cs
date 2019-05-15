789803f3d7a8e18ba55df0aae6de02eefbc60b3a
tuyuri6ka <47937296+tuyuri6ka@users.noreply.github.com>
2019-05-16 03:18:40 +0900

    【追加】PlayerControllerの「走る・歩く」をtransform.positionの記述に変更。CameraControllerに、カメラのキャラへの追従・画面ドラッグにより視点の移動・LeftShiftで視点の切り替えを追加。

--- Shooting/Assets/Scripts/PlayerController.cs
+++ Shooting/Assets/Scripts/PlayerController.cs
@@ -5,44 +5,62 @@ using UnityEngine;
 public class PlayerController : CharaController
 {
 
-    public float walkSpeed;
-	public float runSpeed;
-    public Rigidbody rb;
+	/// <summary>
+	/// 利用する際は、
+	/// 1.CameraTransformにPlayerとMainCameraをアタッチしてください。
+	/// 2.適度にパラメータを調整してください。
+	/// </summary>
+
+    public float runSpeed;				// 移動速度
+	public float walkSpeed;				// 歩・走の速度比の調整
+	public float jumpPower;				// ジャンプ力ぅ…ですかね…
+	public Transform CameraTransform;	// 横の視点移動（MainCameraをアタッチ）
+	public float rotateRate;
 
     PlayerController(){
 		HP = 10;
 		Attack = 1;
-		walkSpeed = 0.01f;
-		runSpeed = 0.03f;
+		runSpeed = 4.0f;
+		walkSpeed = 1.0f;
     }
 
-    void Start(){
-        rb = GetComponent<Rigidbody>();     
+    void Start(){     
     }
 
 	void Update() {
 
-		float horizontal = Input.GetAxis("Horizontal");
+		// キー入力用変数
 		float vertical = Input.GetAxis("Vertical");
+		float horizontal = Input.GetAxis("Horizontal");
 		bool hold = Input.GetKey(KeyCode.LeftShift);
+		bool shot = Input.GetKey(KeyCode.J);
+		bool jump = Input.GetKey(KeyCode.Space);
+		
+		// 進行方向制御用変数
+		float angleDir = CameraTransform.transform.eulerAngles.y * (Mathf.PI / 180.0f);		// ワールド座標に対するオイラー角
+		Vector3 dirVertical = new Vector3(Mathf.Sin(angleDir), 0, Mathf.Cos(angleDir));		// カメラ正面方向をベクトルで取得
+		Vector3 dirHorizontal = new Vector3(Mathf.Cos(angleDir), 0, -Mathf.Sin(angleDir));  // カメラ右正面方向をベクトルで取得
 
-		// 歩く。構え時には動かない(処理も回さない)。
-		// TODO: AddForceはTranslateに切り替え
-		if (!(horizontal == 0 && vertical == 0) && !hold) {
-			float x = horizontal * walkSpeed;
-			float z = vertical * walkSpeed;
-			rb.AddForce(x, 0, z);
+		// Playerの回転制御
+		if (false) {
+
 		}
-		// 構える。"J"で射撃（未実装）。
-		// カメラの視点を変化させる（MainCameraに実装？）。
-		else if (hold) {
+
+		
+		// 走る：デフォルト
+		if (!hold) {
+			this.transform.position += dirVertical * vertical * runSpeed * Time.deltaTime;
+			this.transform.position += dirHorizontal * horizontal * runSpeed * Time.deltaTime;
+		}
+
+		// 構える・歩く。"J"で射撃（未実装）。
+		if (hold) {
+			this.transform.position += dirVertical * vertical * walkSpeed * Time.deltaTime;
+			this.transform.position += dirHorizontal * horizontal * walkSpeed * Time.deltaTime;
 
 			if (Input.GetKey(KeyCode.J)) {
 				Debug.Log("射撃");
 			}
-		} else {
-
 		}
-
 	}
 }
